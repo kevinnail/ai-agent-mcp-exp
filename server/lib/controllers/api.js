70e930cb134ff67e9336343be1ed3c7127df8007
kevinnail <fumalicious@gmail.com>
2025-09-24 22:56:40 -0700

    add list tools call to test api

--- server/lib/controllers/api.js
+++ server/lib/controllers/api.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import { getToolsFromMcpServer } from '../services/McpService.js';
 
 const router = express.Router();
 
 // Sample API endpoint
 router.get('/hello', (req, res) => {
-  res.json({ message: 'Hello from the API!' });
+  const tools = getToolsFromMcpServer();
+
+  res.json({ message: 'Hello from the API!', tools });
 });
 
 // Add more routes here
